0be114e18c85efaaedfaac1622c730b8f588b8f6
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-08-05 10:11:42 +0300

    added response to client

--- src/server.cpp
+++ src/server.cpp
@@ -17,7 +17,6 @@ int main(int argc, char **argv) {
   std::cout << "Logs from your program will appear here!\n";
 
   // Uncomment this block to pass the first stage
-  //
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
    std::cerr << "Failed to create server socket\n";
@@ -53,8 +52,11 @@ int main(int argc, char **argv) {
 
   std::cout << "Waiting for a client to connect...\n";
 
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
+
+  std::string res = "HTTP/1.1 200 OK\r\n\r\n";
+  send(client, res.c_str(), res.length(), 0);
 
   close(server_fd);
 
